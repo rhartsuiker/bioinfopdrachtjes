022341361e1af1a1c0ec5205e3f093e5985bb27c
rhartsuiker <105050525+rhartsuiker@users.noreply.github.com>
2022-11-03 17:25:42 +0100

    Add files via upload

--- alignment.py
+++ alignment.py
@@ -9,49 +9,86 @@ def align_fastas(amino_fasta, out_file):
                                          align=True, output="FASTA")
     clustalw_cline()
 
-def process_alignment(alignment_fasta, ):
+
+def process_alignment(alignment_fasta, nuc_fasta):
     alignment = AlignIO.read(alignment_fasta, "fasta")
-    
+
     print(alignment)
 
     # AA count for each position
-    pos_aa_count = list()
+    scoring_dicts = list()
 
     for i in range(alignment.get_alignment_length()):
         temp = dict()
         unique_aas = set(alignment[:,i])
         for aa in unique_aas:
-            temp[aa] = alignment[:,i].count(aa)
-        pos_aa_count.append(temp)
+            temp[aa] = alignment[:,i].count(aa) / len(alignment[:,i])
+        temp = {k: v for k, v in sorted(temp.items(), key=lambda item: item[1], reverse = True)}
+        scoring_dicts.append(temp)
 
-    # Percentage conservation in each position
-    aa_conserve = [max(d.values()) / len(alignment[:,0]) * 100 for d in pos_aa_count]
-
-    conserv_percent = list()
-    
-    for i,c in enumerate(aa_conserve):
-        print("pos {} has {}% conservation".format(i,int(c)))
-        
     # Scoring
-    # ~ nuc_to_amino()
+    mutated = nuc_to_amino(nuc_fasta).strip("*")
     
+    cost = 0
+    cost_per_pos = list()
+    
+    for aa, d in zip(mutated, scoring_dicts):
+        keys = list(d.keys())
+        
+        pre_cost = cost
+        
+        if aa != keys[0]:
+            cost += d[keys[0]]
+            if len(d) > 1:
+                for key in keys:
+                    if aa == key:
+                        cost -= d[key]
+                        
+        cost_per_pos.append(cost - pre_cost)
+
+    for i,e in enumerate(cost_per_pos):
+        print("Position {} has a cost of {}".format(i,e))
+    print("The total cost for the mutated sequence is: {}".format(cost))
+
+
 def nuc_to_amino(nuc_fasta):
     coding_dna = SeqIO.read(nuc_fasta, "fasta")
     return Seq.translate(coding_dna.seq)
 
 
 def main():
-    nuc_to_amino("/homes/rhartsuiker/bioinf3/hbb/coding_seq.fasta")
-
-    fasta_file = "/homes/rhartsuiker/bioinf3/hbb/hbb_protein_fam.fasta"
+    multi_fasta = "/homes/rhartsuiker/bioinf3/hbb/hbb_protein_fam.fasta"
     alignment_file = "/homes/rhartsuiker/bioinf3/hbb/hbb_protein_aln.fasta"
+    nuc_fasta = "/homes/rhartsuiker/bioinf3/hbb/coding_seq.fasta"
 
-    align_fastas(fasta_file, alignment_file)
+    align_fastas(multi_fasta, alignment_file)
 
-    process_alignment(alignment_file)
+    process_alignment(alignment_file, nuc_fasta)
+    
+# ~ def main(args):
+    # ~ parser = argparse.ArgumentParser(description="Calculate the severity scores for SNPs"
+                                                 # ~ "in a MSA.")
+
+    # ~ parser.add_argument("file", metavar="F", type=str,
+                        # ~ help="The file path to the Multiple sequence alignment, only multi Fasta files")
+
+    # ~ parser.add_argument("-l", metavar="location", type=str,
+                        # ~ help="A single location for the SNP to calculate the severity")
+
+
+    # ~ parser.add_argument("-s", metavar="save", type=str, choices=["amino", "alignment", "all"],
+                        # ~ help="What the program wil save and write to the destination denoted "
+                             # ~ "by -out, you can choose from the following options:"
+                             # ~ "amino; only saves the amino translation,"
+                             # ~ "alignment; only saves the alignment information,"
+                             # ~ "all; saves amino & alignment information."
+                             # ~ "If no option was given, will not save data and continue to only "
+                             # ~ "calculate the single SNP location.")
+
+    # ~ parser.add_argument("-out", metavar="output", type=str,
+                        # ~ help="The path and filename for saving the desired information, "
+                             # ~ "make sure to denote it as an .csv file.")
+
 
 if __name__ == "__main__":
     sys.exit(main())
-    
-# ~ AlignIO.write(alignment, "/homes/rhartsuiker/bioinf3/hbb/hbb_protein_fam.phy", "phylip")
-# ~ AlignIO.write(alignment, "/homes/rhartsuiker/bioinf3/hbb/hbb_protein_fam.sth", "stockholm")
